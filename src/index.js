00d63a323a2f3ebcbb4788091fa049cca5bf1a1b
eibz <60506754+eibz@users.noreply.github.com>
2022-02-01 13:00:25 +0000

    Basic if-else and onclick

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import * as Vue from 'vue/dist/vue.esm-bundler.js';
+
+const app = Vue.createApp({
+    template: `
+      <button v-on:click="increment">Increment</button>
+      <p>{{count}}</p>
+      <div v-if="isEven()">
+        Even
+      </div>
+      <div v-else>
+        Odd
+      </div>
+    `,
+
+    data() {
+      return {
+        count: 0
+      }
+    },
+
+    methods: {
+      increment() {
+        this.count += 1;
+      },
+      isEven() {
+        return this.count % 2 === 0;
+      }
+    }
+});
+app.mount('#app');
