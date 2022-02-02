5803a695f6589d9a150bde61ddc2c0d9e7272be7
AtsMultanen <atsmultanen@gmail.com>
2022-02-02 16:06:36 +0000

    Custom button component

--- form-validation/App.vue
+++ form-validation/App.vue
@@ -1,10 +1,22 @@
 <template>
-    Hi
+    <my-button
+        background="darkslateblue"
+        color="white"
+        :disabled="!valid"
+    />
 </template>
 
 <script>
+import MyButton from './MyButton.vue';
 export default {
-    
+    components: {
+        MyButton
+    },
+    data() {
+        return {
+            valid: false,
+        }
+    }
 }
 </script>
 
---
+++ form-validation/MyButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button
+        v-bind:style="{ background, color }"
+        v-bind:disabled="disabled"
+    >Button</button>
+</template>
+
+<script>
+export default {
+    props: {
+        background: {
+            type: String
+        },
+        color: {
+            type: String
+        },
+        disabled: {
+            type: Boolean
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
