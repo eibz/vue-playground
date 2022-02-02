fcdeae66467b16644c3157db05729cbd9ae3dd79
AtsMultanen <atsmultanen@gmail.com>
2022-02-02 11:58:34 +0000

    Input validation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const app = Vue.createApp({
     template: `
       <button v-on:click="increment">Increment</button>
       <p>{{count}}</p>
+
+      <input 
+        v-bind:value="value" 
+        v-on:input="input"
+      />
+      {{ value }}
+      <div class="red">{{ error }}</div>
+
       <div 
         v-for="number in numbers"
         v-bind:class="getClass(number)"
@@ -12,28 +20,31 @@ const app = Vue.createApp({
          {{ number }} 
         </div>
       </div>
-      <div v-if="isEven()">
-        Even
-      </div>
-      <div v-else>
-        Odd
-      </div>
     `,
 
     data() {
       return {
         count: 0,
         numbers: [1,2,3,4,5,6,7,8,9,10],
+        value: 'user',
       }
     },
 
     computed: {
       evenList() {
         return this.numbers.filter(num => this.isEven(num))
+      },
+      error() {
+        if (this.value.length < 5) {
+          return 'Must be greater than 5.'
+        }
       }
     },
 
     methods: {
+      input($event){
+        this.value = $event.target.value;
+      },
       getClass(number) {
         return this.isEven(number) ? 'blue' : 'red';
       },
