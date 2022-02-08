66d4aabcb4f04021441da1608dbcc978d33c3a29
AtsMultanen <atsmultanen@gmail.com>
2022-02-08 16:23:15 +0000

    Creating a generic card component and passing data into it using slots

--- pokemon/App.vue
+++ pokemon/App.vue
@@ -1,32 +1,42 @@
 <template>
   <div class="cards">
-    <div 
-      class="card"
-      v-for="p in pokemon"
-      :key="p.id"
+    <card
+      v-for="pokemon in pokemons"
+      :key="pokemon.id"
     >
-        <div class="title">
-            {{ p.name }}
-        </div>
+      <template v-slot:title>
+        {{ pokemon.name }}
+      </template>
 
-        <div class="content">
-            <img :src="p.sprite">
-        </div>
+      <template v-slot:content>
+        <img :src="pokemon.sprite">
+      </template>
 
-        <div class="description">
-            <div v-for="type in p.types" :key="type">{{ type }}</div>
+      <template v-slot:description>
+        <div 
+          v-for="type in pokemon.types" 
+          :key="type"
+        >
+          {{ type }}
         </div>
-    </div>
+      </template>
+    </card>
   </div>
 </template>
 
 <script>
+import Card from './Card.vue';
+
 const api = 'https://pokeapi.co/api/v2/pokemon';
 const ids = [1, 4, 7]
 export default {
+    components: {
+      Card
+    },
+
     data() {
       return {
-        pokemon: [],
+        pokemons: [],
       }
     },
 
@@ -45,7 +55,7 @@ export default {
               responses.map( data => data.json() )
             );
 
-            this.pokemon = json.map(datum => ({
+            this.pokemons = json.map(datum => ({
               id: datum.id,
               name: datum.name,
               sprite: datum.sprites.other['official-artwork'].front_default,
@@ -60,41 +70,7 @@ export default {
 .cards {
   display: flex;
 }
-.card {
-  border: 1px solid silver;
-  border-radius: 8px;
-  max-width: 200px;
-  margin: 0 5px;
-  cursor: pointer;
-  box-shadow: 0px 1px 3px darkgrey;
-  transition: 0.2s;
-}
-.title, .content, .description {
-  padding: 16px;
-  text-transform: capitalize;
-  text-align: center;
-}
-.title, .content {
-  border-bottom: 1px solid silver;
-}
-.title {
-  font-size: 1.25em;
-}
-.card:hover {
-  transition: 0.2s;
-  box-shadow: 0px 1px 9px darkgrey;
-}
-
 img {
   width: 100%;
-}
-.cards {
-  display: flex;
-}
-.opace {
-  opacity: 0.5;
-}
-.card:hover {
-  opacity: 1;
 }
 </style>
