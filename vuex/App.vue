f598002fdec68b21a6db3886a206af97d450d1bb
AtsMultanen <atsmultanen@gmail.com>
2022-02-14 16:53:52 +0000

    Basic state management

---
+++ vuex/App.vue
@@ -0,0 +1,28 @@
+<template>
+    {{ store.state.count }}
+    <button @click="click">Increment</button>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+
+export default {
+    setup() {
+        const store = useStore();
+        console.log(store);
+
+        const click = () => {
+            store.commit('increment', 10)
+        }
+
+        return {
+            store,
+            click,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
