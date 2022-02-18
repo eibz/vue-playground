3178f2fee2ce6e425fbaf8565ab08fbdde9cfbe9
eibz <atsmultanen@gmail.com>
2022-02-18 11:31:04 +0000

    Basic setup + layout

---
+++ photos/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <layout>
+        <template v-slot:header>
+
+        </template>
+        <template v-slot:sidebar>
+
+        </template>
+        <template v-slot:content>
+
+        </template>
+    </layout>
+</template>
+
+<script>
+import Layout from './Layout.vue';
+export default {
+    components: {
+        Layout,
+    }
+}
+</script>
+
+<style>
+*  {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+}
+</style>
